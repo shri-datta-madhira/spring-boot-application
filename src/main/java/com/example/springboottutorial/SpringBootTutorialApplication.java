5448686bd20acb6647496041b0c179623817c2ef
Shri Datta Madhira <madhira.s@northeastern.edu>
2023-09-25 21:48:23 -0500

    Prime Numbers

--- src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
+++ src/main/java/com/example/springboottutorial/SpringBootTutorialApplication.java
@@ -1,5 +1,9 @@
 package com.example.springboottutorial;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -17,6 +21,26 @@ public class SpringBootTutorialApplication {
   @GetMapping("/hello")
   public String sayHello (@RequestParam(value="myName", defaultValue = "World") String name) {
     return String.format("Hello %s!!", name);
+  }
+
+  @GetMapping("/primes")
+  public List<Integer> getPrimes (@RequestParam(value="upperBound", defaultValue = "10") int upperBound) {
+    List<Integer> primes = new ArrayList<>();
+
+    if (upperBound <= 1) return Collections.emptyList();
+
+    for (int x=1; x<=upperBound; x++) {
+      if (x==1 || x==0) continue;
+      int flg = 1;
+      for (int y=2; y<=x/2; ++y) {
+        if (x % y == 0) {
+          flg = 0;
+          break;
+        }
+      }
+      if (flg == 1) primes.add(x);
+    }
+    return primes;
   }
 
 }
